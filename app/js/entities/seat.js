a05e1629691532b3d473de05888a803d3cccb60b
handal95 <45425838+handal95@users.noreply.github.com>
2021-06-08 04:26:01 +0900

    seed seat complete

--- app/js/entities/seat.js
+++ app/js/entities/seat.js
@@ -1,5 +1,5 @@
 import { fill_code } from "../../utils/string.js"
-import { insert_record } from "../db/insert.js"
+import { insert_seed } from "../db/insert.js"
 import { select_data } from "../db/select.js"
 
 export async function get_seats(conn, req){
@@ -42,9 +42,9 @@ export async function insert_screen_seat(conn, screen_info) {
         }
         
         for (let c = 1; c <= cols_length; c++){
-            const row_code = fill_code(`${r}`, '0', 2)
-            const col_code = fill_code(`${c}`, '0', 2)
-            const seat_uid = `${SCREEN_CODE}_S${row_code}${col_code}`
+            const row_code = fill_code(`${r}`, '0', 3)
+            const col_code = fill_code(`${c}`, '0', 3)
+            let seat_uid = seat_code_to_uid(`${SCREEN_CODE}${row_code}${col_code}`)            
 
             if(seat_category == "PRIME"){
                 if( c <= 2 || c > cols_length - 2){
@@ -53,10 +53,24 @@ export async function insert_screen_seat(conn, screen_info) {
             }
 
             let query_body = (
-                `VALUES('${seat_uid}', '${SCREEN_CODE}', '${seat_category}', '${r}', '${c}')`
+                `'${seat_uid}', '${SCREEN_CODE}', '${seat_category}', '${seat_status}', '${r}', '${c}'`
             )
-            await insert_record(conn, "SEAT", )
+            await insert_seed(conn, "SEAT", query_body)
 
         }
     }
+}
+
+// temp function
+function seat_code_to_uid(raw_code, uid){
+    let seat_uid = raw_code.replace(/_/gi, "")
+    // THEATER PREFIX
+    seat_uid = seat_uid.replace(/T/, 1)
+
+    // SCREEN CATEGORY PREFIX
+    seat_uid = seat_uid.replace(/N/, 1)
+    seat_uid = seat_uid.replace(/X/, 2)
+    seat_uid = seat_uid.replace(/I/, 3)
+
+    return seat_uid
 }
--- app/js/process/seeder.js
+++ app/js/process/seeder.js
@@ -38,35 +38,6 @@ export async function seed_seat(req) {
     return is_success
 }
 
-export async function seed_seat(req) {
-    let is_success = false
-
-    // DB 연결
-    const conn = await getDBConnect()
-    try {
-        // 스키마 불러오기
-        for(let i = 0; i < table.length; i++){
-            let schema_path = `${BASE_PATH}${table[i]}.txt`
-            let query_bodies = fs.readFileSync(schema_path).toString().split("\r\n");
-
-            for(let j = 0; j < query_bodies.length; j++){
-                // 데이터 주입
-                await insert_seed(conn, table[i], query_bodies[j])
-            }
-        }
-        
-        is_success = true
-
-    } catch (err) {
-        console.log("(create) : Process is failed by ", err)
-        is_success = false
-
-    } finally {
-        await doDBRelease(conn)
-    }
-    
-    return is_success
-} 
 
 
 export async function seed_data(req) {
