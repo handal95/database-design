210bdc9efa51e70bfffb1df74cb164015794e435
handal95 <45425838+handal95@users.noreply.github.com>
2021-06-08 04:02:46 +0900

    reverted code cleansing

--- app/queries/schema/seat.sql
+++ app/queries/schema/seat.sql
@@ -4,20 +4,8 @@ seat_uid NUMBER(16, 0) NOT NULL
 screen_code VARCHAR(16) NOT NULL,
 seat_category VARCHAR(16),
 seat_status VARCHAR(16) NOT NULL,
-<<<<<<< HEAD
-<<<<<<< HEAD
-seat_row VARCHAR(4) NOT NULL
-seat_col VARCHAR(4) NOT NULL
-=======
 seat_row VARCHAR(4) NOT NULL,
 seat_col VARCHAR(4) NOT NULL,
->>>>>>> parent of 1cc2259 (add seed seat)
-=======
-seat_row NUMBER(4) NOT NULL
-    CONSTRAINT SEAT_ROW_CK CHECK(seat_row >= 1),
-seat_col NUMBER(4) NOT NULL
-    CONSTRAINT SEAT_COL_CK CHECK(seat_col >= 1),
->>>>>>> parent of ec31891 (fix seat)
 
 CONSTRAINT SEAT_PK
     PRIMARY KEY(seat_uid),
