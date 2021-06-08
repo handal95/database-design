9110553ec1527d4c030604be7c58888aefce5737
handal95 <45425838+handal95@users.noreply.github.com>
2021-06-08 09:49:08 +0900

    ticketing process

--- app/queries/schema/ticket.sql
+++ app/queries/schema/ticket.sql
@@ -1,7 +1,7 @@
 ticket_uid NUMBER(16, 0) NOT NULL
     CONSTRAINT TICKET_UID_CK CHECK(ticket_uid >= 0),
 
-payment_uid NUMBER(16, 0) NOT NULL,
+payment_uid NUMBER(16, 0),
 ticket_price NUMBER(8, 0) NOT NULL
     CONSTRAINT TICKET_PRICE_CK CHECK(ticket_price >= 0),
 adult_no NUMBER(2, 0) DEFAULT 0 NOT NULL
